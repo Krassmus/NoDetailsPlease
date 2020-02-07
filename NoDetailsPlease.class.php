50ea3f7bec991c35e68a26fd2b82a03aa8b64422
Rasmus Fuhse <fuhse@data-quest.de>
2020-02-07 16:11:12 +0100

    bugfix studip 3.5

--- NoDetailsPlease.class.php
+++ NoDetailsPlease.class.php
@@ -5,7 +5,7 @@ class NoDetailsPlease extends StudIPPlugin implements SystemPlugin
     public function __construct()
     {
         parent::__construct();
-        if (!$GLOBALS['perm']->have_perm("user") && (stripos($_SERVER['REQUEST_URI'], "dispatch.php/course/details") !== false)) {
+        if (!$GLOBALS['perm']->have_perm("autor") && (stripos($_SERVER['REQUEST_URI'], "dispatch.php/course/details") !== false)) {
             throw new AccessDeniedException();
         }
     }
