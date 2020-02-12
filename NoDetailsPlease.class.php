148b54d09707fd5525c07bb6253dc3abe24c04e0
Rasmus Fuhse <fuhse@data-quest.de>
2020-02-12 14:29:33 +0100

    bugfix LoginException

--- NoDetailsPlease.class.php
+++ NoDetailsPlease.class.php
@@ -4,10 +4,13 @@ require_once 'lib/exceptions/LoginException.php';
 
 class NoDetailsPlease extends StudIPPlugin implements SystemPlugin
 {
+    protected $already = false;
+
     public function __construct()
     {
         parent::__construct();
-        if (!$GLOBALS['perm']->have_perm("autor") && (stripos($_SERVER['REQUEST_URI'], "dispatch.php/course/details") !== false)) {
+        if (!$GLOBALS['NO_DETAILS_PLEASE_ALREADY_STARTED'] && !$GLOBALS['perm']->have_perm("autor") && (stripos($_SERVER['REQUEST_URI'], "dispatch.php/course/details") !== false)) {
+            $GLOBALS['NO_DETAILS_PLEASE_ALREADY_STARTED'] = true;
             throw new LoginException();
         }
     }
