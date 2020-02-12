914b4d8cf719d757589927d04017b1729e8cf8a9
Rasmus Fuhse <fuhse@data-quest.de>
2020-02-12 13:03:20 +0100

    throw LoginException

--- NoDetailsPlease.class.php
+++ NoDetailsPlease.class.php
@@ -1,12 +1,14 @@
 <?php
 
+require_once 'lib/exceptions/LoginException.php';
+
 class NoDetailsPlease extends StudIPPlugin implements SystemPlugin
 {
     public function __construct()
     {
         parent::__construct();
         if (!$GLOBALS['perm']->have_perm("autor") && (stripos($_SERVER['REQUEST_URI'], "dispatch.php/course/details") !== false)) {
-            throw new AccessDeniedException();
+            throw new LoginException();
         }
     }
 }
